5d03bb3c34d8de6873c63951884de7329a796b8d
HARSH SHARMA <harsh159357@gmail.com>
2020-06-14 01:09:22 +0530

    Initial commit with webview for PlayStation Controller in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MaterialApp(home: WebViewExample(), debugShowCheckedModeBanner: false));
+
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PlayStation Controller'),
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: 'https://harsh159357.github.io/app_demos/index.html',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+        );
+      }),
+    );
+  }
+}
