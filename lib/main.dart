9e21f477c9e10df796beaeba51d7b8bd71d94e59
HARSH SHARMA <harsh159357@gmail.com>
2020-06-14 02:19:47 +0530

    Reverted back the app bar change

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(MaterialApp(home: WebViewExample(), debugShowCheckedModeBanner: false));
-
+void main() => runApp(
+    MaterialApp(home: WebViewExample(), debugShowCheckedModeBanner: false));
 
 class WebViewExample extends StatefulWidget {
   @override
@@ -17,7 +17,9 @@ class _WebViewExampleState extends State<WebViewExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text("PlayStation Controller"),
+      ),
       body: Builder(builder: (BuildContext context) {
         return WebView(
           initialUrl: 'https://harsh159357.github.io/app_demos/index.html',
